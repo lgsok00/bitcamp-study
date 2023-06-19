a57e76d863cd337acf370916541cd4b23248820e
lgsok00 <lgsok00@naver.com>
2023-06-19 14:23:18 +0900

    project20

--- myapp/app/src/main/java/bitcamp/util/LinkedList.java
+++ myapp/app/src/main/java/bitcamp/util/LinkedList.java
@@ -128,4 +128,12 @@ public class LinkedList {
     }
     return false;
   }
+
+  // static 중첩 클래스
+  static class Node {
+    // 자바 최상위 객체 (Object) 타입으로 선언
+    Object value;
+    // Node 가 정의되기 전 선언해도 문제 없음
+    Node next;
+  }
 }

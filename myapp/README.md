e787007aa39784ccfdc08372d9d85af469e2def2
lgsok00 <lgsok00@naver.com>
2023-09-01 18:08:07 +0900

    Spring IoC Container + Mybatis

--- myapp/README.md
+++ myapp/README.md
@@ -381,3 +381,47 @@
 ## 61. 네이버 클라우드의 Image Optimizer를 이용하여 썸네일 이미지 다루기
 
 - 네이버 클라우드의 Image Optimizer 사용법
+
+## 62. forward/include/refresh/redirect 다루기
+
+- 오류 메시지 출력에 forward 및 refresh 사용하기
+- 상단 메뉴 및 하단 정보 출력에 include 사용하기
+- 등록 완료 후 목록 페이지로 이동할 때 redirect 사용하기
+
+## 63. 쿠키와 ServletContext 보관소 활용하기
+
+- 쿠키를 이용하여 로그인 할 때 입력한 이메일을 보관하기
+- 서블릿들이 공통으로 사용하는 객체를 ServletContext 보관소에 보관하기
+
+## 64. JSP를 이용하여 MVC 모델1 구조로 변경하기
+
+- MVC 모델1 특징 이해
+- JSP 구동 원리 이해 및 사용법
+- EL 사용법
+- JSTL 사용법
+
+## 65. 서블릿을 결합하여 MVC 모델2 구조로 변경하기
+
+- MVC 모델2 특징 이해
+- 서블릿과 JSP의 역할 및 구동 원리
+
+## 66. Front Controller 디자인 패턴 도입하기
+
+- Front Controller 디자인 패턴의 효과 및 적용 방법
+- 프론트 컨트롤러와 페이지 컨트롤러의 역할 이해
+
+## 67. 페이지 컨트롤러를 POJO로 전환하기
+
+- 페이지 컨트롤러를 POJO 클래스로 변경
+  POJO? Plain Old Java Object (그냥 일반 자바 문법으로 만든 클래스)
+- 의존 객체 주입하기
+
+## 68. Spring IoC 컨테이너를 이용하여 페이지 컨트롤러를 관리하기
+
+- Spring IoC 컨테이너를 이용하여 객체를 자동 생성하는 방법
+- 의존 객체를 자동으로 주입하는 방법
+- Spring IoC 컨테이너에 들어 있는 객체를 꺼내 사용하는 방법
+
+## 69. Mybatis + Spring IoC 컨테이너 = DAO 구현체 자동 생성
+
+- Mybatis의 Spring 연동 플러그인을 사용하여 DAO를 자동 생성하는 방법
---
+++ myapp/app-69-server/src/main/java/bitcamp/myapp/controller/LoginController.java
@@ -0,0 +1,51 @@
+package bitcamp.myapp.controller;
+
+import bitcamp.myapp.dao.MemberDao;
+import bitcamp.myapp.vo.Member;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component("/auth/login")
+public class LoginController implements PageController {
+
+  MemberDao memberDao;
+
+  public LoginController(MemberDao memberDao) {
+    this.memberDao = memberDao;
+  }
+
+  @Override
+  public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+    if (request.getMethod().equals("GET")) {
+      return "/WEB-INF/jsp/auth/form.jsp";
+    }
+
+
+    String email = request.getParameter("email");
+    String password = request.getParameter("password");
+
+    // Cookie
+    if (request.getParameter("saveEmail") != null) {
+      Cookie cookie = new Cookie("email", email);
+      response.addCookie(cookie);
+    } else {
+      Cookie cookie = new Cookie("email", "no");
+      cookie.setMaxAge(0);
+      response.addCookie(cookie);
+    }
+
+    Member loginUser = memberDao.findByEmailAndPassword(email, password);
+
+    if (loginUser == null) {
+      request.setAttribute("refresh", "2;url=/app/auth/login");
+      throw new Exception("회원 정보가 일치하지 않습니다.");
+    }
+
+    // 로그인 정보를 다른 요청에서도 사용할 있도록 세션 보관소에 담아 둔다.
+    request.getSession().setAttribute("loginUser", loginUser);
+    return "redirect:/";
+  }
+}

11e5fc25385e454fe4e231e2706c302de8f6e246
lgsok00 <lgsok00@naver.com>
2023-09-05 16:44:50 +0900

    PageController의 파라미터 다루기

---
+++ myapp/app-77-server/src/main/java/bitcamp/myapp/controller/RequestMapping.java
@@ -0,0 +1,9 @@
+package bitcamp.myapp.controller;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME) // 컴파일 할 때 바이트 코드에 삽입, 실행 중 추출
+public @interface RequestMapping {
+  String value() default "";
+}

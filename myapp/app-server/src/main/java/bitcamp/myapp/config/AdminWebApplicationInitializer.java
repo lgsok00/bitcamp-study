01321d9d8da022c2cf38b9e11d231005e6cb291c
lgsok00 <lgsok00@naver.com>
2023-09-06 15:51:01 +0900

    Spring WebMVC

---
+++ myapp/app-server/src/main/java/bitcamp/myapp/config/AdminWebApplicationInitializer.java
@@ -0,0 +1,41 @@
+package bitcamp.myapp.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.MultipartConfigElement;
+import javax.servlet.ServletRegistration;
+
+public class AdminWebApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+  public AdminWebApplicationInitializer() {
+    System.out.println("AdminWebApplicationInitializer 생성됨!");
+  }
+
+  @Override
+  protected String getServletName() {
+    return "admin";
+  }
+
+  @Override
+  protected Class<?>[] getRootConfigClasses() {
+    // ContextLoaderListener의 IoC Container가 사용할 java config 클래스를 지정한다.
+    return null;
+  }
+
+  @Override
+  protected Class<?>[] getServletConfigClasses() {
+    // DispatcherServlet의 IoC Container가 사용할 java config 클래스를 지정한다.
+    return new Class[] {AdminConfig.class};
+  }
+
+  @Override
+  protected String[] getServletMappings() {
+    // DispatcherServlet의 URL을 지정한다.
+    return new String[] {"/app/*"};
+  }
+
+  @Override
+  protected void customizeRegistration(ServletRegistration.Dynamic registration) {
+    registration.setMultipartConfig(new MultipartConfigElement("temp", 10000000, 15000000, 1000000));
+  }
+}
